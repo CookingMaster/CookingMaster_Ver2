020dbcb29a951c64e1e5f904cce53b1ce6d42bfe
R821-B06\game <rsp44316@gmail.com>
2018-10-09 15:31:19 +0900

    条件文修正

--- project/src/Class/ResourceManager.hpp
+++ project/src/Class/ResourceManager.hpp
@@ -336,7 +336,7 @@ private:
 			}
 			SetUseASyncLoadFlag(TRUE); // ”ñ“¯Šú“Ç‚İ‚İƒtƒ‰ƒOON
 			sounds_[name] = LoadSoundMem(path.c_str());
-			if (sounds_[name] = -1)
+			if (sounds_[name] == -1)
 			{
 				DOUT << path + " is failed" << std::endl;
 				assert(false);
@@ -353,7 +353,7 @@ private:
 			switch (CheckHandleASyncLoad(sounds_[name]))
 			{
 			case -1:
-				if (sounds_[name] = -1)
+				if (sounds_[name] == -1)
 				{
 					DOUT << name + "loadAsync is failed" << std::endl;
 					assert(false);
