c5ed89d0a02e3daa9b3cdb132741576a208f1bfb
yokota0717 <17ci0139@gmail.com>
2018-10-18 11:48:18 +0900

    プレイヤ制御コンポーネント途中

--- project/src/Components/PlayerController.hpp
+++ project/src/Components/PlayerController.hpp
@@ -17,14 +17,11 @@
 
 namespace ECS {
 	/**
-	* @brief プレイヤの女の子を制御するコンポーネント
-	* - AnimatorByBPMが必要です
+	* @brief プレイヤの状態を保持する
 	*/
-	class PlayerController final : public ComponentSystem
+	struct PlayerState final : public ComponentData
 	{
-	public:
-		//!状態を管理する
-		enum class State
+		enum class State : short
 		{
 			Non,	//!未定義
 			Idle,	//!立ち
@@ -36,11 +33,98 @@ namespace ECS {
 			Enter,	//!入場
 			Clear,	//!曲終了
 		};
+		State val;
+		explicit PlayerState() : val(State::Non) {}
+	};
+	/**
+	* @brief プレイヤ専用アニメーター
+	* - SpriteAnimationDraw、PlayerStateが必要
+	*/
+	class AnimatorPlayer final : public ComponentSystem
+	{
 	private:
-		State state_ = State::Non;
-		AnimatorByBPM* animator_ = nullptr;
+		SpriteAnimationDraw* animation_ = nullptr;
+		PlayerState* state_;
+		float bpm_ = 0;
+		DWORD beat_ = 0;
+		DWORD start_ = 0;
+		std::string soundname_ = "";
+		Counter counter_;
+		int frame_ = 0;		//!アニメーションするフレーム数
 		int indexX_ = 0;
 		int indexY_ = 0;
+		int xmin_ = 0;		//!描画する画像のX方向のインデックスの最小
+		int ymin_ = 0;		//!描画する画像のY方向のインデックスの最小
+		int maxXnum_ = 0;	//!描画する画像のX方向の枚数
+		int maxYnum_ = 0;	//!描画する画像のY方向の枚数
+
+	public:
+		AnimatorPlayer(const char* soundname, const float bpm, const int frame) 
+			:
+			soundname_(soundname),
+			bpm_(bpm),
+			frame_(frame)
+		{
+			calcBeat();
+		}
+		void initialize() override
+		{
+			animation_ = &entity->getComponent<SpriteAnimationDraw>();
+			state_ = &entity->getComponent<PlayerState>();
+		}
+		void update() override
+		{
+			switch (state_->val)
+			{
+			case PlayerState::State::Idle:
+
+				break;
+			case PlayerState::State::Left:
+				break;
+			case PlayerState::State::Right:
+				break;
+			case PlayerState::State::Up:
+				break;
+			case PlayerState::State::Down:
+				break;
+			case PlayerState::State::Enter:
+				break;
+			case PlayerState::State::Clear:
+				break;
+			case PlayerState::State::Non:
+			default:
+				break;
+			}
+		}
+		//!BPMを設定する
+		void setBPM(const float bpm)
+		{
+			bpm_ = bpm;
+		}
+		/**
+		* @brief 1拍の長さ(ms)を計算する
+		* @note 1000(ms) * 60(sec/min) / bpm(beat/min)
+		*/
+		void calcBeat()
+		{
+			beat_ = static_cast<DWORD>(1000 * (60.f / bpm_));
+		}
+
+	};
+
+
+	/**
+	* @brief プレイヤの女の子を制御するコンポーネント
+	* - PlayerStateが必要
+	*/
+	class PlayerController final : public ComponentSystem
+	{
+	private:
+		AnimatorPlayer* animator_ = nullptr;
+		PlayerState * state_;
+		int indexX_ = 0;
+		int indexY_ = 0;
+		Counter animCounter_;
 	public:
 		PlayerController()
 		{
@@ -48,53 +132,61 @@ namespace ECS {
 		}
 		void initialize() override
 		{
-			animator_ = &entity->getComponent<AnimatorByBPM>();
+			if (!entity->hasComponent<AnimatorPlayer>())
+			{
+				entity->addComponent<PlayerState>();
+			}
 		}
 		void update() override
 		{
 			tryAction();
 			actionByState();
+			
 		}
 		//!描画する画像のインデックスを設定する
 		void setGraphIndex()
 		{
 
 		}
+		/**
+		* @brief 現在の状態に合わせて入力を受け付ける
+		* @note 要するにゲープロのThink()
+		*/
 		void tryAction()
 		{
-			switch (state_)
+			switch (state_->val)
 			{
-			case State::Idle:
+			case PlayerState::State::Idle:
 				if (Input::Get().getKeyFrame(KEY_INPUT_LEFT) == 1)
 				{
-					changeState(State::Left);
+					changeState(PlayerState::State::Left);
 				}
 				if (Input::Get().getKeyFrame(KEY_INPUT_RIGHT) == 1)
 				{
-					changeState(State::Right);
+					changeState(PlayerState::State::Right);
 				}
 				if (Input::Get().getKeyFrame(KEY_INPUT_UP) == 1)
 				{
-					changeState(State::Up);
+					changeState(PlayerState::State::Up);
 				}
 				if (Input::Get().getKeyFrame(KEY_INPUT_DOWN) == 1)
 				{
-					changeState(State::Down);
+					changeState(PlayerState::State::Down);
 				}
 				break;
-			case State::Left:
+			case PlayerState::State::Left:
 				break;
-			case State::Right:
+			case PlayerState::State::Right:
 				break;
-			case State::Up:
+			case PlayerState::State::Up:
 				break;
-			case State::Down:
+			case PlayerState::State::Down:
 				break;
-			case State::Enter:
+			case PlayerState::State::Enter:
 				break;
-			case State::Clear:
+			case PlayerState::State::Clear:
 				break;
-			case State::Non:
+			case PlayerState::State::Non:
 			default:
 				break;
 
@@ -103,23 +195,24 @@ namespace ECS {
 		}
 		void actionByState()
 		{
-			switch (state_)
+			switch (state_->val)
 			{
-			case State::Idle:
+			case PlayerState::State::Idle:
+
 				break;
-			case State::Left:
+			case PlayerState::State::Left:
 				break;
-			case State::Right:
+			case PlayerState::State::Right:
 				break;
-			case State::Up:
+			case PlayerState::State::Up:
 				break;
-			case State::Down:
+			case PlayerState::State::Down:
 				break;
-			case State::Enter:
+			case PlayerState::State::Enter:
 				break;
-			case State::Clear:
+			case PlayerState::State::Clear:
 				break;
-			case State::Non:
+			case PlayerState::State::Non:
 			default:
 				break;
 
@@ -127,9 +220,17 @@ namespace ECS {
 
 		}
 
-		void changeState(State state)
+		void changeState(PlayerState::State state)
 		{
-			state_ = state;
+			state_->val = state;
+		}
+
+		/**
+		* @brief アニメーション用のカウンタを設定する
+		*/
+		void initAnimCounter(const int add, const int max)
+		{
+			animCounter_.SetCounter(0, add, 0, max);
 		}
 	};
 
