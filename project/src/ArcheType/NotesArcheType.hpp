d92e25510e47a60a93cca8dc58987495b3a6e95f
feveleK5563 <buncha102828@gmail.com>
2018-10-19 11:19:56 -0700

    タイミングよくノーツが生成、削除されるようになったよやったね！！！

--- project/src/ArcheType/NotesArcheType.hpp
+++ project/src/ArcheType/NotesArcheType.hpp
@@ -8,6 +8,7 @@
 #pragma once
 #include "../GameController/GameController.h"
 #include "../Components/Renderer.hpp"
+#include "../Components/ReplayPhysics.hpp"
 #include "../Class/NotesAndScoreData.hpp"
 
 namespace ECS
@@ -22,21 +23,23 @@ namespace ECS
 			if (dir == OneNoteData::Direction::LEFT)
 			{
 				entity->addComponent<Transform>().setPosition(-32.f, float(System::SCREEN_HEIGHT) / 2.f);
-				entity->addComponent<Velocity>(5, 0);
+				entity->addComponent<Velocity>(10, 0);
 			}
 			else
 			{
 				entity->addComponent<Transform>().setPosition(float(System::SCREEN_WIDIH), float(System::SCREEN_HEIGHT) / 2.f);
-				entity->addComponent<Velocity>(-5, 0);
+				entity->addComponent<Velocity>(-10, 0);
 			}
 			entity->addComponent<Gravity>(0.f);
 			entity->addComponent<Physics>();
+
 			entity->addComponent<Color>();
 			entity->addComponent<AlphaBlend>();
 
 			entity->addComponent<SimpleDraw>(notesData.imageName.c_str());
 
-			entity->addComponent<KillEntity>(arrivalBeatTime);
+			entity->addComponent<KillEntity>(int(arrivalBeatTime));
+			entity->addComponent<ReplayPhysics>(int(wait));
 
 			entity->addGroup(ENTITY_GROUP::LAYER1);
 			return entity;
---
+++ project/src/Components/ReplayPhysics.hpp
@@ -0,0 +1,37 @@
+/**
+* @file ReplayPhysics.hpp
+* @brief 停止しているPhysicsとKillEntityを再開させるコンポーネント
+* @author feveleK5563
+* @date 2018/10/18
+*/
+
+#pragma once
+#include "../ECS/ECS.hpp"
+#include "../Components/BasicComponents.hpp"
+
+namespace ECS
+{
+	class ReplayPhysics final : public ComponentSystem
+	{
+	private:
+		int cnt_;
+	public:
+		ReplayPhysics(const int span) :cnt_(span) {}
+
+		void initialize() override
+		{
+			entity->stopComponent<Physics>();
+			entity->stopComponent<KillEntity>();
+		}
+		void update() override
+		{
+			--cnt_;
+			if (cnt_ <= 0)
+			{
+				entity->updateComponent<Physics>();
+				entity->updateComponent<KillEntity>();
+				//entity->stopComponent<ReplayPhysics>();
+			}
+		}
+	};
+}
