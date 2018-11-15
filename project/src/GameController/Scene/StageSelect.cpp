910109b4fe2d06e5b0008253a7a05ea9d6ab0209
tonarinohito <rsp44316@gmail.com>
2018-11-15 10:03:59 +0900

    ソース追加

---
+++ project/src/GameController/Scene/StageSelect.cpp
@@ -0,0 +1,25 @@
+﻿#include "StageSelect.h"
+#include "../src/Input/Input.hpp"
+namespace Scene
+{
+	StageSelect::StageSelect(IOnSceneChangeCallback* sceneTitleChange, [[maybe_unused]] Parameter* parame, ECS::EntityManager* entityManager)
+		: AbstractScene(sceneTitleChange)
+		, entitytManager_(entityManager)
+	{
+
+	}
+	void StageSelect::update()
+	{
+		if (Input::Get().getKeyFrame(KEY_INPUT_X) == 1)
+		{
+			auto playerDetail = std::make_unique<Parameter>();
+			playerDetail->add<std::string>("名前", "たかし");
+			getCallBack().onSceneChange(SceneName::GAME, playerDetail.get(), StackPopFlag::POP, true);
+		}
+	}
+	void StageSelect::draw()
+	{
+		DrawFormatString(0, 0, 0xffffffff, "セレクト画面");
+	}
+
+}
