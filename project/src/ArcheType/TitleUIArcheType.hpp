e327df96a132e57ca641775ea56821621d584659
feveleK5563 <buncha102828@gmail.com>
2018-11-22 10:55:30 +0900

    ノーツのアニメーション修正

--- project/src/ArcheType/TitleUIArcheType.hpp
+++ project/src/ArcheType/TitleUIArcheType.hpp
@@ -8,7 +8,7 @@ namespace ECS
 	{
 		static Entity* CreateLogoArchetype(const std::string& imgName, const Vec2& pos, EntityManager& entityManager_)
 		{
-			auto* entity = &entityManager_.addEntityAddTag("unko");
+			auto* entity = &entityManager_.addEntity();
 			entity->addComponent<FlashImage>();
 
 			entity->addComponent<Position>(pos);
