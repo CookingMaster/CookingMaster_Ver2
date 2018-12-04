f7e6f492dc6fee49fde95522465ca873ee4a2c6e
tonarinohito <rsp44316@gmail.com>
2018-12-04 14:30:28 +0900

    セレクトシーン修正

---
+++ project/src/ArcheType/SelectArchetype.hpp
@@ -0,0 +1,15 @@
+﻿/**
+* @file SelectArcheType.hpp
+* @brief リザルトで生成するスコアエンティティの原型を作成します
+* @author tonarinohito
+* @date 2018/12/04
+*/
+
+#pragma once
+#include "../GameController/GameController.h"
+#include "../Components/Renderer.hpp"
+
+namespace ECS
+{
+	
+}
