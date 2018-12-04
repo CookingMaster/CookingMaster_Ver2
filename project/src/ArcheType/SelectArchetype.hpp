3504e7ca1ea0906772e6d13c1ae4ecfa3c055c9f
tonarinohito <rsp44316@gmail.com>
2018-12-04 16:25:50 +0900

    Render修正

--- project/src/ArcheType/SelectArchetype.hpp
+++ project/src/ArcheType/SelectArchetype.hpp
@@ -8,8 +8,20 @@
 #pragma once
 #include "../GameController/GameController.h"
 #include "../Components/Renderer.hpp"
-
+#include "../Components/CursorMove.hpp"
 namespace ECS
 {
-	
+	//!カーソルエンティティ
+	static Entity* CreateCursor(const char* graphicName, const Vec2 pos, EntityManager& entityManager_)
+	{
+		auto* entity = &entityManager_.addEntity();
+		entity->addComponent<Transform>().setPosition(pos.x, pos.y);
+		entity->addComponent<Color>();
+		entity->addComponent<AlphaBlend>();
+		entity->addComponent<SpriteDraw>(graphicName);
+		entity->getComponent<SpriteDraw>().setPivot(Vec2{ 32,32 });
+		entity->getComponent<CursorMove>();
+		entity->addGroup(ENTITY_GROUP::UI);
+		return entity;
+	}
 }
---
+++ project/src/Components/CursorMove.hpp
@@ -0,0 +1,40 @@
+﻿/**
+* @file CursorMove.hpp
+* @brief セレクト画面で使うCursorの機能です
+* @author tonarinohito
+* @date 2018/12/04
+*/
+#pragma once
+#include "../ECS/ECS.hpp"
+#include "../Components/Renderer.hpp"
+#include "../Utility/Vec.hpp"
+#include <DxLib.h>
+
+namespace ECS
+{
+	/*
+	@brief カーソルの動きです
+	@details Transform,SpriteDrawが必要です
+	*/
+	class CursorMove final :public ComponentSystem
+	{
+	private:
+		Transform* trans;
+		SpriteDraw* sprite;
+	public:
+		CursorMove()
+		{
+
+		}
+		void initialize() override
+		{
+			entity->addComponent<Transform>();
+			entity->addComponent<SpriteDraw>();
+		}
+		void update() override
+		{
+
+		}
+
+	};
+}
