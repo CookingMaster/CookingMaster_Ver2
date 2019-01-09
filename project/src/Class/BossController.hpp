5e85c5bdd17e791cfc855f3c1dc081c6d3b8319f
tonarinohito <rsp44316@gmail.com>
2019-01-09 18:10:20 +0900

    おやっさんの座標修正

--- project/src/Class/BossController.hpp
+++ project/src/Class/BossController.hpp
@@ -39,7 +39,7 @@ public:
 		ResourceManager::GetGraph().loadDiv("Resource/image/chef_face.png", "BossFace", 2,2, 1, 86, 70);
 		ResourceManager::GetGraph().load("Resource/image/number2.png", "number");//かり
 		ResourceManager::GetGraph().load("Resource/image/pause_button.png", "balloon");//かり
-		bossBody_ = ECS::GameEffectsArcheType::CreateBossBody("Boss", Vec2{100.f,250.f}, &manager_);
+		bossBody_ = ECS::GameEffectsArcheType::CreateBossBody("Boss", Vec2{100.f,235.f}, &manager_);
 		bossFace_ = ECS::GameEffectsArcheType::CreateBossFace("BossFace", Vec2{ 10.f,12.f }, &manager_);
 		bossBody_->getComponent<ECS::Canvas>().addChild(bossFace_);
 		bossFace_->getComponent<ECS::SpriteAnimationDraw>().drawDisable();
